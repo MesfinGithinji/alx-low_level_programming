7a5593299bddcb5bd6ed9474e51d9b4d2a7000db
MesfinGithinji <mesfingithinji@gmail.com>
2023-02-16 06:47:43 -0800

    Next c prog

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  a C program that prints and  compile using -Wall gcc option.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
