d646f7b2a6918cf5ae87fb654117e8f5d327ee6f
MesfinGithinji <mesfingithinji@gmail.com>
2023-02-16 06:39:11 -0800

    Check progrma and betty has to accept

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints exact text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
