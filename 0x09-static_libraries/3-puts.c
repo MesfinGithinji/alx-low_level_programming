4a244e46f0c3accf74d5d9e4d375c433fabae443
MesfinGithinji <mesfingithinji@gmail.com>
2023-03-10 17:59:13 +0300

    archive files and library creation

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+
+}
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
