7241fa2f5ffa61e8d83d1dfe34dd9646919e107f
MesfinGithinji <mesfingithinji@gmail.com>
2023-03-14 03:19:04 +0300

    Concat to free grid again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,15 +18,12 @@ char *str_concat(char *s1, char *s2)
 
 
 
-	if (s1 == NULL)
+	if(s1 == NULL)
+	s1 = "";
 
-		s1 = "";
-
-	if (s2 == NULL)
-
-		s2 = "";
-
-		i = ci = 0;
+	if(s2 == NULL)
+	s2 = "";
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 
@@ -62,15 +59,15 @@ char *str_concat(char *s1, char *s2)
 
 	{
 
-		conct[i] = s2[ci];
+		cnct[i] = s2[ci];
 
 		i++, ci++;
 
 	}
 
-	conct[i] = '\0';
+	cnct[i] = '\0';
 
-	return (conct);
+	return (cnct);
 
 }
 
