2bf31d4b4013be5bf1bbfaa2e49a2fdb7ebf6aa3
MesfinGithinji <mesfingithinji@gmail.com>
2023-03-14 21:44:19 +0300

    betty is boring

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,68 +13,33 @@ int **alloc_grid(int width, int height)
 {
 
 	int **myarr;
-
 	int x, y;
 
-
-
 	if (width <= 0 || height <= 0)
-
 		return (NULL);
-
-
-
 	myarr = malloc(sizeof(int *) * height);
 
-
-
 	if (myarr == NULL)
-
 		return (NULL);
-
-
-
 	for (x = 0; x < height; x++)
-
 	{
-
 		myarr[x] = malloc(sizeof(int) * width);
-
-
-
 		if (myarr[x] == NULL)
-
 		{
-
 			for (; x >= 0; x--)
 
 				free(myarr[x]);
 
-
-
 			free(myarr);
 
 			return (NULL);
-
 		}
-
 	}
-
-
-
 	for (x = 0; x < height; x++)
-
 	{
-
 		for (y = 0; y < width; y++)
-
 			myarr[x][y] = 0;
-
 	}
-
-
-
 	return (myarr);
-
 }
 
