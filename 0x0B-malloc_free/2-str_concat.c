d73da87a123911177cc3eb30049db080e61ea54f
MesfinGithinji <mesfingithinji@gmail.com>
2023-03-14 21:35:22 +0300

    Betty is still petty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,14 +9,9 @@
  */
 
 char *str_concat(char *s1, char *s2)
-
 {
-
 	char *cnct;
-
 	int i, ci;
-
-
 
 	if (s1 == NULL)
 	s1 = "";
@@ -26,21 +21,12 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 
 	while (s1[i] != '\0')
-
 		i++;
-
 	while (s2[ci] != '\0')
-
 		ci++;
-
 	cnct = malloc(sizeof(char) * (i + ci + 1));
-
-
-
 	if (cnct == NULL)
-
 		return (NULL);
-
 	i = ci = 0;
 
 	while (s1[i] != '\0')
