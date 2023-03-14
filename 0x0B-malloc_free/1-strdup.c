732215456d2e597dd66678cda2c7b33714977bd2
MesfinGithinji <mesfingithinji@gmail.com>
2023-03-14 03:02:21 +0300

    store string copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+
+{
+
+	char *mwg;
+
+	int i, r = 0;
+
+
+
+	if (str == NULL)
+
+		return (NULL);
+
+	i = 0;
+
+	while (str[i] != '\0')
+
+		i++;
+
+
+
+	mwg = malloc(sizeof(char) * (i + 1));
+
+
+
+	if (mwg == NULL)
+
+		return (NULL);
+
+
+
+	for (r = 0; str[r]; r++)
+
+		mwg[r] = str[r];
+
+
+
+	return (mwg);
+
+}
+
