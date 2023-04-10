1f5fd2e4c2fa502a4048ce7b640adb1a261b28ed
MesfinGithinji <meshgithinji@gmail.com>
2023-04-10 13:58:06 +0300

    File I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,99 @@
+#include "main.h"
+
+/**
+ * create_buffer - Creates a buffer and allocates 1024 bytes.
+ * @file: Name of file buffer is going to store for.
+ *
+ * Return: Pointer to the buffer created.
+ */
+
+char *create_buffer(char *file)
+{
+char *buffer;
+
+buffer = malloc(sizeof(char) * 1024);
+
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO, "Error writing to %s\n !!", file);
+exit(99);
+}
+
+return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+
+void close_file(int fd)
+{
+int c;
+
+c = close(fd);
+
+if (c == -1)
+{
+dprintf(STDERR_FILENO, "Error closing fd %d\n", fd);
+exit(100);
+}
+}
+
+
+/**
+ * main - Copiy contents of a file to another.
+ * @argc: Number of arguments parsed.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If argument count is incorrect - exit code 97.
+ *              If file_from doesn;t exist or can't be read - exit code 98.
+ *              If file_to can't be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
+
+int main(int argc, char *argv[])
+{
+int from, to, r, w;
+char *buffer;
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+buffer = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+do {
+if (from == -1 || r == -1)
+{
+dprintf(STDERR_FILENO, "Error:File %s\n Cant be Read!!", argv[1]);
+free(buffer);
+exit(98);
+}
+
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
+{
+dprintf(STDERR_FILENO, "Error:Can't write to %s\n", argv[2]);
+free(buffer);
+exit(99);
+}
+
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
+
+} while (r > 0);
+
+free(buffer);
+close_file(from);
+close_file(to);
+
+return (0);
+}
